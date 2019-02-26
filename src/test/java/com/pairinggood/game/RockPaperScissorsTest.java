a3665e6867d7f8dda416e428407186a51448f77c
Andrew <letzerich@gmail.com>
2019-02-26 10:51:41 -0600

    scissors beat paper

--- src/test/java/com/pairinggood/game/RockPaperScissorsTest.java
+++ src/test/java/com/pairinggood/game/RockPaperScissorsTest.java
@@ -2,32 +2,44 @@ package com.pairinggood.game;
 
 import org.junit.Test;
 
+import static com.pairinggood.game.RockPaperScissors.SCISSORS;
 import static org.junit.Assert.assertEquals;
 
 public class RockPaperScissorsTest {
 
     @Test
-    public void play_RockThenScissor_RockBeatsScissors() {
+    public void play_RockThenScissors_RockBeatsScissors() {
         //arange
         RockPaperScissors game = new RockPaperScissors();
 
         //act
-        String winner = game.play("rock", "scissors");
+        String winner = game.play("rock", SCISSORS);
 
         //assert
         assertEquals("rock", winner);
     }
 
     @Test
-    public void play_ScissorThenRock_RockBeatsScissors() {
+    public void play_ScissorsThenRock_RockBeatsScissors() {
         //arange
         RockPaperScissors game = new RockPaperScissors();
 
         //act
-        String winner = game.play("scissors", "rock");
+        String winner = game.play(SCISSORS, "rock");
 
         //assert
         assertEquals("rock", winner);
     }
 
+    @Test
+    public void play_PaperThenScissors_ScissorsBeatsPaper() {
+        //arange
+        RockPaperScissors game = new RockPaperScissors();
+
+        //act
+        String winner = game.play("paper", SCISSORS);
+
+        //assert
+        assertEquals(SCISSORS, winner);
+    }
 }
